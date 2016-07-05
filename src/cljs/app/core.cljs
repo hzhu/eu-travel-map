ea4fc629db46f4decf5babaaa201e93705633c5b
hzhu <hz@henryzhu.me>
2016-07-05 00:46:29 -0700

    Refactored out d3 code that generates eu map from topojson into it's own function `create-eu-map`

--- src/cljs/app/core.cljs
+++ src/cljs/app/core.cljs
@@ -1,10 +1,33 @@
 (ns app.core
   (:require
    [reagent.core :as reagent]
+   [data.core :as data]
    [cljsjs.d3 :as d3]
-   [cljsjs.topojson :as topojson]
-   [data.core :as data]))
+   [cljsjs.topojson :as topojson]))
 (enable-console-print!)
+
+(defn create-eu-map [ratom path svg]
+  (.. js/d3
+    (json "eu.json"
+          (fn [eu-data]
+            (let [topjson-transform (.. js/topojson (feature eu-data (aget eu-data "objects" "europe")) -features)]
+              (.. svg
+                  (selectAll "country")
+                  (data topjson-transform)
+                  enter
+                  (append "path")
+                  (attr "d" path)
+                  (attr "class" "country")
+                  (attr "data-name" (fn [d]
+                                      (aget d "properties" "name")))
+                  (style "stroke" "black")
+                  (style "opacity" 0.5)
+                  (style "fill" "black")
+                  (style "fill" (fn [d]
+                                  (let [data (@ratom :countries)
+                                        country (aget d "properties" "name")]
+                                      (data country))))))))))
+
 
 (defn did-mount-fn [ratom]
   (let [width 800
@@ -18,32 +41,14 @@
               (append "svg")
               (attr "height" height)
               (attr "width" width))]
-  
-    (.. js/d3
-        (json "eu.json" (fn [eu-data] 
-                          (let [topjson-transform (.-features (.feature js/topojson eu-data (aget eu-data "objects" "europe")))]
-                            (.. svg                              
-                                (selectAll "country")
-                                (data topjson-transform)
-                                enter
-                                (append "path")
-                                (attr "d" path)
-                                (attr "class" "country")                           
-                                (attr "data-name" (fn [d]                                                  
-                                                    (aget d "properties" "name")
-                                                    ))
-                                (style "stroke" "black")
-                                (style "opacity" 0.5)
-                                (style "fill" "black")
-                                (style "fill" (fn [d]
-                                                (let [data (@ratom :countries)
-                                                      country (aget d "properties" "name")]
-                                                    (data country)))))))))))
+    (create-eu-map ratom path svg)))
+
 
 (defn eu-map [ratom]
-  (reagent/create-class { 
+  (reagent/create-class {
       :reagent-render (fn [] [:div {:id "map"}])
       :component-did-mount #(did-mount-fn ratom)}))
+
 
 (defn ^:export main []
   (reagent/render [eu-map data/app-state]
